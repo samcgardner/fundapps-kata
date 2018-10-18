8c3f16f8e3d27cfd475f57ac83c500a4f4049587
Sam Gardner <samcgardner@gmail.com>
2018-10-18 16:49:03 +0100

    Add no-op implementation of F and B and failing test

--- src/main/kotlin/com/samcgardner/rover/Commands.kt
+++ src/main/kotlin/com/samcgardner/rover/Commands.kt
@@ -1,9 +1,17 @@
 package com.samcgardner.rover
 
-data class Rover(val foo: String)
-data class Command (val foo : String)
+data class Rover(val x : Int, val y : Int, val heading : Direction)
+enum class Command{F, B}
+enum class Direction{N, S, E, W}
 
-fun move(rover : Rover, commands : Sequence<Command>) : Rover {
+fun move(rover : Rover, commands : Sequence<String>) : Rover {
+    // Don't check for or catch an exception here - if there is an error in our command sequence, we'd like to know
+    // about it _before_ we start moving our rover around
+    val parsedCommands = commands.map { Command.valueOf(it) }
+    return parsedCommands.fold(rover, { myRover, command  -> applyCommand(myRover, command)})
+}
+
+fun applyCommand(rover : Rover, command : Command) : Rover {
     return rover
 }
 
--- src/test/kotlin/com/samcgardner/rover/Commands.kt
+++ src/test/kotlin/com/samcgardner/rover/Commands.kt
@@ -1,12 +1,44 @@
 package com.samcgardner.rover
 
+import junit.framework.Assert.assertEquals
 import org.junit.Test
 
 class Commands {
 
     @Test
-    fun moves() {
-        assert(true)
+    fun movesNorth() {
+        val initialRover = Rover(0, 0, Direction.N)
+        val movedRover = applyCommand(initialRover, Command.F)
+        assertEquals(0, movedRover.x)
+        assertEquals(1, movedRover.y)
+        assertEquals(Direction.N, movedRover.heading)
+    }
+
+    @Test
+    fun movesEast() {
+        val initialRover = Rover(0, 0, Direction.N)
+        val movedRover = applyCommand(initialRover, Command.F)
+        assertEquals(0, movedRover.x)
+        assertEquals(1, movedRover.y)
+        assertEquals(Direction.N, movedRover.heading)
+    }
+
+    @Test
+    fun movesSouth() {
+        val initialRover = Rover(0, 0, Direction.N)
+        val movedRover = applyCommand(initialRover, Command.F)
+        assertEquals(0, movedRover.x)
+        assertEquals(1, movedRover.y)
+        assertEquals(Direction.N, movedRover.heading)
+    }
+
+    @Test
+    fun movesWest() {
+        val initialRover = Rover(0, 0, Direction.N)
+        val movedRover = applyCommand(initialRover, Command.F)
+        assertEquals(0, movedRover.x)
+        assertEquals(1, movedRover.y)
+        assertEquals(Direction.N, movedRover.heading)
     }
 
 }
